7a556540b318194f249d7cc0b6ae90e32ee19121
Seth Kingry <s.kingry@modcloth.com>
2013-02-19 20:34:25 -0800

    Pull and Put on the filer instead of local or S3

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures rbenv"
+maintainer        "ModCloth, Inc."
+maintainer_email  "platformsphere@modcloth.com"
+license           "Apache 2.0"
+description       "Installs and Configures rbenv"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version           "0.2.0"
+
+depends           "autofs"
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,14 +9,21 @@
 
 # traverse users in data bag and see if they set a ruby attribute
 # (or some other query) and install rubies
+
+include_recipe "autofs::smartos"
+domain = node.domain
+
 search(:users, node['rbenv']['users_query']) do |u|
   rbenv_user = u['username'] ||= u['id']
+  rbenv_group = u['group']
   rbenv_user_dir = "/home/#{rbenv_user}"
   rubies = u['ruby']
   rbenv_dir = "#{rbenv_user_dir}/.rbenv"
 
-  bash 'change permissions' do
-    code "chown -R #{rbenv_user}:#{rbenv_user} #{rbenv_user_dir}"
+  bash "change permisions" do
+    code <<-EOH
+       chown -R #{rbenv_user}:#{rbenv_group} #{rbenv_user_dir}
+    EOH
   end
 
   git rbenv_dir do
@@ -39,7 +46,6 @@ search(:users, node['rbenv']['users_query']) do |u|
       source "bashrc"
       action :create
     end
-
     cookbook_file "#{rbenv_user_dir}/.bash_profile" do
       owner rbenv_user
       mode '0744'
@@ -60,15 +66,12 @@ search(:users, node['rbenv']['users_query']) do |u|
   end
 
   rubies.each do |ruby|
-    # Fix me a hash would be better
-
-    Chef::Log.info"INSTALL RUBY ===================> #{ruby}"
-
+    # Fix me a hash would be better  
     extra_flags =" export CFLAGS='-DBYTE_ORDER -DLITTLE_ENDIAN' \
                    export ac_cv_func_dl_iterate_phdr=no \
                    export rb_cv_have_signbit=no " if ruby.eql?("1.9.3-p327")
-
-    bash "install #{ruby}" do
+    
+    bash "installing ruby #{ruby}" do
       user rbenv_user
       cwd rbenv_user_dir
       code <<-EOH
@@ -78,30 +81,35 @@ search(:users, node['rbenv']['users_query']) do |u|
         export CONFIGURE_OPTS='--with-opt-dir=/opt/local'
         # ruby compile flags to link correctly for smartos
         export LDFLAGS="-R/opt/local -L/opt/local/lib "
+        export FILER="/net/filer.#{domain}/export/ModCloth/shared02/installations/rbenv"
         #{extra_flags}
         source .bashrc
 
         if ( which ruby && ( rbenv versions | grep #{ruby} ) ) &>/dev/null
-          then echo "#{ruby} already installed";
+          then echo "#{ruby} already installed!";
           echo > /tmp/ruby_installed
-        elif [ -f $HOME/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz ];
-          then echo "copying #{ruby}  from LOCAL directory..." >> /tmp/copy && mkdir -p  $HOME/.rbenv/versions &&  \
-          tar -xzf $HOME/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz -C $HOME/.rbenv/versions
+        elif [ -f $FILER/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz ];
+          then echo "copying ruby from LOCAL directory..." > /tmp/copy && mkdir -p $HOME/.rbenv/versions && \
+          tar -xzf $FILER/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz -C $HOME/.rbenv/versions
         else
           # make sure to create os/version folder for ruby
-          [  -d $HOME/smartos-base64-1.7.1/#{rbenv_user} ] || echo "creating pkg directory..." && mkdir -p $HOME/smartos-base64-1.7.1/#{rbenv_user}
+          [ -d $FILER/smartos-base64-1.7.1/#{rbenv_user} ] || echo "creating pkg directory..." && \
+          mkdir -p $FILER/smartos-base64-1.7.1/#{rbenv_user}
           echo "installing ruby #{ruby} from source..." && \
           rbenv install #{ruby} && echo "creating tar file" && cd .rbenv/versions/ && \
-          mkdir -p $HOME/smartos-base64-1.7.1/#{rbenv_user} && \
-          tar -czf $HOME/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz #{ruby};
+          mkdir -p $FILER/smartos-base64-1.7.1/#{rbenv_user} && \
+          tar -czf $FILER/smartos-base64-1.7.1/#{rbenv_user}/#{ruby}.tar.gz #{ruby};
         fi
+        
         rbenv rehash
         rbenv global #{ruby}
-        if  rbenv which bundle; then
+        
+        if rbenv which bundle; then
           echo 'bundler already installed'
         else
           gem install bundler
         fi
+        
         rbenv rehash
       EOH
     end
