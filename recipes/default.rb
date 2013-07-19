c8330afc30648df33a1eb07bedb34c3071effb59
Dan Buch <d.buch@modcloth.com>
2013-07-19 16:30:11 -0400

    Renaming the provider call for renamed cookbook plus removing bogus files
    that were breaking cookbook upload.

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,7 +10,7 @@
 include_recipe "autofs::smartos"
 default_ruby = node.rbenv.default_ruby
 
-rbenv "rbenv admin #{default_ruby}" do
+modcloth_rbenv "rbenv admin #{default_ruby}" do
   user "admin"
   ruby "#{default_ruby}"
   action :install
