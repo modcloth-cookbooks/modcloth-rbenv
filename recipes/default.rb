b0c251bd99fe6454cd515c6448d488cecad639fa
Marcos Sainz <m.sainz@modcloth.com>
2013-07-19 17:34:38 -0700

    Parametrized default_user (which used to be hard-coded as 'admin'). That way we can override via default or overriden attrs.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,10 +8,11 @@
 #
 
 include_recipe "autofs::smartos"
+default_user = node.rbenv.default_user
 default_ruby = node.rbenv.default_ruby
 
-modcloth_rbenv "rbenv admin #{default_ruby}" do
-  user "admin"
+modcloth_rbenv "rbenv #{default_user} #{default_ruby}" do
+  user "#{default_user}"
   ruby "#{default_ruby}"
   action :install
 end
